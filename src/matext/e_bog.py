02c4703b89865f62db4f53f41fe11d00d7a8c2d0
Søren Højlund Mollerup <shm@dbc.dk>
2018-06-12 13:35:26 +0200

    script til at finde bøger med ebogs link

---
+++ src/matext/e_bog.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# -*- mode: python -*-
+"""
+Høster pidder med publizon provider id
+"""
+import logging
+import requests
+from tqdm import tqdm
+logger = logging.getLogger(__name__)
+logging.getLogger("requests").setLevel(logging.WARNING)
+logging.getLogger("urllib3").setLevel(logging.WARNING)
+
+
+def fetch_pids(solr_url, limit=None, rows=10):
+    fetched = 0
+    end = limit
+    if not end:
+        end = rows + 1
+
+    params = {'q': 'rec.collectionIdentifier:150015-e*',
+              'start': 0,
+              'rows': rows,
+              'fl': 'rec.repositoryId'}
+
+    while params['start'] < end:
+        r = requests.get(solr_url.rstrip('/') + '/query', params=params)
+        if not r.ok:
+            r.raise_for_status()
+        if not limit:
+            end = limit = r.json()['response']['numFound']
+
+        params['start'] += params['rows']
+
+        docs = r.json()['response']['docs']
+        for doc in docs:
+            if fetched >= end:
+                break
+            yield doc['rec.repositoryId']
+            fetched += 1
+
+
+def get_pids(solr_url, limit=None, outfile='ebogs_pids.txt'):
+    with open(outfile, 'w') as fh:
+        for pid in tqdm(fetch_pids(solr_url, limit=limit)):
+            fh.write(pid+'\n')
+
+
+def cli():
+    """ Commandline interface """
+    import argparse
+
+    solr_url = 'http://cisterne-solr.dbc.dk:8984/solr/corepo_20180330_1818_stored/'
+
+    parser = argparse.ArgumentParser(description='høst af pid\'er med ebogsr')
+    parser.add_argument('-l', '--limit', dest='limit', type=int,
+                        help='limits number of harvested reviews)', default=None)
+    parser.add_argument('-o', '--outfile', dest='outfile',
+                        help='file to dump pids into. default is \'ebogs_pids.txt\'',
+                        default='forlagsbreskrivelse_pids.txt')
+    parser.add_argument('-v', '--verbose', dest='verbose', action='store_true',
+                        help='verbose output')
+    args = parser.parse_args()
+
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.DEBUG)
+    get_pids(solr_url, limit=args.limit, outfile=args.outfile)
+
+
+if __name__ == '__main__':
+    cli()
